6a7cd3264b3e84a05a17b5f3c23589229f104bbf
Charlie Turner <cturner@igalia.com>
2019-04-18 18:01:17 +0100

    Update framework, plugins, ocdm

--- package/wpe/wpeframework-cdmi/wpeframework-cdmi-clearkey/wpeframework-cdmi-clearkey.mk
+++ package/wpe/wpeframework-cdmi/wpeframework-cdmi-clearkey/wpeframework-cdmi-clearkey.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_CDMI_CLEARKEY_VERSION = 3fb37d8ee90a8af941a6ebeff6309f89ead60fd4
+WPEFRAMEWORK_CDMI_CLEARKEY_VERSION = 026c6fb9e809ddfaf2d3487cf0107158d4452b26
 WPEFRAMEWORK_CDMI_CLEARKEY_SITE_METHOD = git
 WPEFRAMEWORK_CDMI_CLEARKEY_SITE = git@github.com:WebPlatformForEmbedded/OCDM-Clearkey.git
 WPEFRAMEWORK_CDMI_CLEARKEY_INSTALL_STAGING = YES
--- package/wpe/wpeframework/wpeframework.mk
+++ package/wpe/wpeframework/wpeframework.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_VERSION = 6445a0eaf10c20e39236633cba75a4131c4b0a7f 
+WPEFRAMEWORK_VERSION = 58ef727e5ba90b1918fc3aba6234253d04fa3927
 WPEFRAMEWORK_SITE = $(call github,WebPlatformForEmbedded,WPEFramework,$(WPEFRAMEWORK_VERSION))
 WPEFRAMEWORK_INSTALL_STAGING = YES
 WPEFRAMEWORK_DEPENDENCIES = zlib $(call qstrip,$(BR2_PACKAGE_SDK_INSTALL))
