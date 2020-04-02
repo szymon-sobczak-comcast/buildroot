89106a8cdd1b17c432c95344965d2f003ae6835d
Damian Danylko <d.danylko@metrological.com>
2020-04-02 11:43:35 +0200

    [Explora] Remove IR key jason mapping file

--- board/explora/post-build.sh
+++ board/explora/post-build.sh
@@ -13,12 +13,6 @@ if [ -f "${BOARD_DIR}/index.html" ]; then
 	cp -pf "${BOARD_DIR}/index.html" "${TARGET_DIR}/www/"
 fi
 
-# FIXME: Copy keymap for Kaon remote
-if [ -f "${BOARD_DIR}/kaon-ir-remote.json" ]; then
-	mkdir -p "${TARGET_DIR}/usr/share/WPEFramework/RemoteControl/"
-	cp -pf "${BOARD_DIR}/kaon-ir-remote.json" "${TARGET_DIR}/usr/share/WPEFramework/RemoteControl/ir-remote.json"
-fi
-
 # Create links for PlayReady
 mkdir -p "${TARGET_DIR}/etc/playready/"
 mkdir -p "${TARGET_DIR}/root/OCDM/"
