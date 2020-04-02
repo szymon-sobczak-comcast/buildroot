2eaedbc6d2cd11b6656af782c87b6690e814aaff
Damian Danylko <d.danylko@metrological.com>
2020-04-02 12:08:27 +0200

    [Explora] Add MC specific web-remote.json maping file

--- board/explora/post-build.sh
+++ board/explora/post-build.sh
@@ -13,6 +13,12 @@ if [ -f "${BOARD_DIR}/index.html" ]; then
 	cp -pf "${BOARD_DIR}/index.html" "${TARGET_DIR}/www/"
 fi
 
+# Copy MC specific web-remote.json
+if [ -f "${BOARD_DIR}/web-remote.json" ]; then
+       mkdir -p "${TARGET_DIR}/usr/share/WPEFramework/RemoteControl/"
+       cp -pf "${BOARD_DIR}/web-remote.json" "${TARGET_DIR}/usr/share/WPEFramework/RemoteControl/web-remote.json"
+fi
+
 # Create links for PlayReady
 mkdir -p "${TARGET_DIR}/etc/playready/"
 mkdir -p "${TARGET_DIR}/root/OCDM/"
