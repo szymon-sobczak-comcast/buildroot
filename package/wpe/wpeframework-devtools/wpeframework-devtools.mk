275f569a5a12a56d8c0787a5c62df546035039bc
anjalirajan <anjali.r@tataelxsi.co.in>
2020-05-12 17:25:46 +0530

    Changes to integrate unit test to buildroot.

--- package/wpe/wpeframework-devtools/wpeframework-devtools.mk
+++ package/wpe/wpeframework-devtools/wpeframework-devtools.mk
@@ -21,4 +21,11 @@ endif
 ifeq ($(BR2_PACKAGE_WPEFRAMEWORK_TEST_UNITTESTS),y)
 WPEFRAMEWORK_DEPENDENCIES += gtest
 WPEFRAMEWORK_CONF_OPTS += -DBUILD_TESTS=ON
+#install the test binaries
+define WPEFRAMEWORK_INSTALL_UNITTESTS
+    $(INSTALL) -D -m 0755 $(@D)/Tests/tests/WPEFramework_test_tests $(TARGET_DIR)/usr/bin/WPEFramework_test_tests
+    $(INSTALL) -D -m 0755 $(@D)/Tests/core/WPEFramework_test_core $(TARGET_DIR)/usr/bin/WPEFramework_test_core
+    $(INSTALL) -D -m 0755 $(WPEFRAMEWORK_PKGDIR)/run-wpeframework_unittests.sh $(TARGET_DIR)/usr/share/WPEFramework/run-wpeframework_unittests.sh
+endef
+WPEFRAMEWORK_POST_INSTALL_TARGET_HOOKS += WPEFRAMEWORK_INSTALL_UNITTESTS
 endif
