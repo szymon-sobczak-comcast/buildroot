793583be68f340bfd5387c4cdb0d6efbe5956735
anjalirajan <anjali.r@tataelxsi.co.in>
2020-05-12 17:54:57 +0530

    Updated the script & config.

--- package/wpe/wpeframework/run-wpeframework_unittests.sh
+++ package/wpe/wpeframework/run-wpeframework_unittests.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+DIRECTORY="/root/test_coverage"
+if [ ! -d $DIRECTORY ]; then
+  mkdir $DIRECTORY
+fi
+export GCOV_PREFIX=$DIRECTORY
+export GCOV_PREFIX_STRIP=7
+
 /usr/bin/WPEFramework_test_tests
 /usr/bin/WPEFramework_test_core
