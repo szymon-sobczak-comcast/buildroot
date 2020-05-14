1198b7d0603646fc45e2baad3da537c6983cceae
msieben <github@desiderium.eu>
2020-05-14 16:26:27 +0200

    [wpeframwork]: Bump to version on development/RPI4

--- package/wpe/wpeframework/wpeframework.mk
+++ package/wpe/wpeframework/wpeframework.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_VERSION = bc116648ebacfb600ea9e69d0b5a775426d0d85c
+WPEFRAMEWORK_VERSION = 6fc62568080047cc68b0eaf2ae482956420824a2
 WPEFRAMEWORK_SITE = $(call github,rdkcentral,Thunder,$(WPEFRAMEWORK_VERSION))
 WPEFRAMEWORK_INSTALL_STAGING = YES
 WPEFRAMEWORK_DEPENDENCIES = zlib $(call qstrip,$(BR2_PACKAGE_SDK_INSTALL)) host-wpeframework-tools
