50d2c854d81cb2c329c58c4a4a18d3972e5abec1
Damian Danylko <d.danylko@metrological.com>
2020-08-03 13:34:29 +0200

    Bump Netflix hash: Switch for Netflix 5.2.3

--- package/netflix52/netflix52.mk
+++ package/netflix52/netflix52.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NETFLIX52_VERSION = cbec47dc14254e08e0eb74ee46f95e52386ebff9
+NETFLIX52_VERSION = a4e51a899418c9d80a3329dd5306d2e5440250a2
 NETFLIX52_SITE = git@github.com:Metrological/netflix.git
 NETFLIX52_SITE_METHOD = git
 NETFLIX52_LICENSE = PROPRIETARY
