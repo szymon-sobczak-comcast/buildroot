451fe32987070791c79c54920c95dd9028ce34c5
Damian Danylko <d.danylko@metrological.com>
2020-08-03 13:33:51 +0200

    Bump Netflix Plugin version

--- package/wpe/wpeframework-netflix/wpeframework-netflix.mk
+++ package/wpe/wpeframework-netflix/wpeframework-netflix.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_NETFLIX_VERSION = 9b6906c57d9ff8a76d4f8e760107ea296ad5f51e
+WPEFRAMEWORK_NETFLIX_VERSION = 4630b6cc16667a5ab243ab5b1f0941eeff0b7158
 ifeq ($(BR2_PACKAGE_NETFLIX5),y)
 # Netflix 5 has a little different API, use "netflix5" branch for now.
 WPEFRAMEWORK_NETFLIX_CONF_OPTS += -DNETFLIX_VERSION_5=true
