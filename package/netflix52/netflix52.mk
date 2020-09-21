6bb498032eabeb26f3aef73b205ef002fb409e3f
Damian Danylko <d.danylko@metrological.com>
2020-09-21 17:38:15 +0200

    Revert "Bump Netflix and gst-sink: Changes for video frame peeking"
    This reverts commit 3ab26bcb792b8363613867964ea49eeea7cc26b6.

--- package/netflix52/netflix52.mk
+++ package/netflix52/netflix52.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NETFLIX52_VERSION = 851e4192713b1a826749be021f507a5632677904
+NETFLIX52_VERSION = 9b47cc05c9e8f0247541189cbe4b7958e376c4ed
 NETFLIX52_SITE = git@github.com:Metrological/netflix.git
 NETFLIX52_SITE_METHOD = git
 NETFLIX52_LICENSE = PROPRIETARY
