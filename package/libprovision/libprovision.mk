e44553aa9f45381a79a81bfec834f54ee803cd29
Divya-563 <divdus@gmail.com>
2021-09-15 01:09:46 -0700

    Updated wpe hashes to R3

--- package/libprovision/libprovision.mk
+++ package/libprovision/libprovision.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBPROVISION_VERSION = eda8220965f4f080c4634a04dd51950cf3a8bd48
+LIBPROVISION_VERSION = R3
 LIBPROVISION_SITE_METHOD = git
 LIBPROVISION_SITE = git@github.com:Metrological/libprovision.git
 LIBPROVISION_LICENSE = PROPRIETARY
