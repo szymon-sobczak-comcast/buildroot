44a5c68d87f7fec431ef8cbfdc0a791dc4457b60
Pierre Wielders <pierre@wielders.net>
2021-09-23 16:49:32 +0200

    [XIONE-SDK] Prepare for the prebuild kernel modules from the SDK.

--- package/xione-sdk/xione-sdk.mk
+++ package/xione-sdk/xione-sdk.mk
@@ -12,16 +12,19 @@ XIONE_SDK_INSTALL_TARGET = YES
 define XIONE_SDK_INSTALL_STAGING_CMDS
 	$(INSTALL) -d $(STAGING_DIR)/usr/include
 	$(INSTALL) -d $(STAGING_DIR)/usr/lib
-        cp -rP $(@D)/mali/include $(STAGING_DIR)/usr/include
+        cp -rP $(@D)/mali/include/* $(STAGING_DIR)/usr/include
 	cp -rP $(@D)/mali/lib/* $(STAGING_DIR)/usr/lib
 endef
 
 define XIONE_SDK_INSTALL_TARGET_CMDS
+	$(INSTALL) -d $(TARGET_DIR)/lib
+	$(INSTALL) -d $(TARGET_DIR)/lib/modules
+	$(INSTALL) -d $(TARGET_DIR)/lib/modules/4.9.119
 	$(INSTALL) -d $(TARGET_DIR)/tas
 	$(INSTALL) -d $(TARGET_DIR)/usr/lib
 	$(INSTALL) -m 0640 $(@D)/vision/tee/* $(TARGET_DIR)/tas
 	$(INSTALL) -m 0640 $(@D)/vision/lib/* $(TARGET_DIR)/usr/lib
-	cp -P $(@D)/mali/lib/* $(TARGET_DIR)/usr/lib
+	cp -Pf $(@D)/mali/lib/* $(TARGET_DIR)/usr/lib || :
 endef
 
 define QORVO_BUILD_MODULE
