43d64a961839034d4030008c29d6172d3562c72f
Pierre Wielders <pierre@wielders.net>
2021-10-01 15:05:41 +0200

    [BUMP] Moving the Thunder repositories to the latest greatest and including Broadcom stuff from different locations.

--- package/wpe/wpeframework-rdkservices/wpeframework-rdkservices.mk
+++ package/wpe/wpeframework-rdkservices/wpeframework-rdkservices.mk
@@ -3,8 +3,7 @@
 # wpeframework-rdkservices
 #
 ################################################################################
-
-WPEFRAMEWORK_RDKSERVICES_VERSION = R3
+WPEFRAMEWORK_RDKSERVICES_VERSION = 01cf2faa3561ec0c8aad9f8032cc7a941da2cb62
 WPEFRAMEWORK_RDKSERVICES_SITE_METHOD = git
 WPEFRAMEWORK_RDKSERVICES_SITE = git@github.com:WebPlatformForEmbedded/ThunderNanoServicesRDK.git
 WPEFRAMEWORK_RDKSERVICES_INSTALL_STAGING = YES
--- package/wpe/wpeframework-tools/wpeframework-tools.mk
+++ package/wpe/wpeframework-tools/wpeframework-tools.mk
@@ -3,8 +3,7 @@
 # wpeframework-tools
 #
 ################################################################################
-WPEFRAMEWORK_TOOLS_VERSION = R3
-
+WPEFRAMEWORK_TOOLS_VERSION = dc3e567e29fa77fd1b4eb5a395f5b8dd67e499a6
 HOST_WPEFRAMEWORK_TOOLS_SITE = $(call github,rdkcentral,Thunder,$(WPEFRAMEWORK_TOOLS_VERSION))
 HOST_WPEFRAMEWORK_TOOLS_INSTALL_STAGING = YES
 HOST_WPEFRAMEWORK_TOOLS_INSTALL_TARGET = NO
