92dad7070b91ff258869dffc15880f27e887a1ee
k.plata <k.plata@metrological.com>
2021-10-19 09:07:03 +0200

    Bump version of gst1-cencdecrypt and fix install

--- package/gstreamer1/gst1-cencdecrypt/gst1-cencdecrypt.mk
+++ package/gstreamer1/gst1-cencdecrypt/gst1-cencdecrypt.mk
@@ -1,16 +1,7 @@
-GST1_CENCDECRYPT_VERSION = 80dd18e9f6217021b245119c615ae4cf021ac489
+GST1_CENCDECRYPT_VERSION = dad6b3e32783f8c127eddf824813d8e9725f579f
 GST1_CENCDECRYPT_SITE = git@github.com:WebPlatformForEmbedded/gstcencdecryptor.git
 GST1_CENCDECRYPT_SITE_METHOD = git
 GST1_CENCDECRYPT_INSTALL_STAGING = YES
-GST1_CENCDECRYPT_DEPENDENCIES = gstreamer1 wpeframework
-
-define GST1_CENCDECRYPT_INSTALL_STAGING_CMDS
-	$(INSTALL) $(@D)/libgstcencdecrypt.so $(STAGING_DIR)/usr/lib/libgstcencdecrypt.so
-endef
-
-define GST1_CENCDECRYPT_INSTALL_TARGET_CMDS
-	$(INSTALL) $(@D)/libgstcencdecrypt.so $(TARGET_DIR)/usr/lib/libgstcencdecrypt.so
-	ln -sf ../libgstcencdecrypt.so $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstcencdecrypt.so
-endef
+GST1_CENCDECRYPT_DEPENDENCIES = gstreamer1 wpeframework wpeframework-clientlibraries wpeframework-plugins 
 
 $(eval $(cmake-package))
