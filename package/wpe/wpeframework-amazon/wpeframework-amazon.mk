010d991647fd5700e84437ed061510f89d9ad795
Divya-563 <divdus@gmail.com>
2022-03-15 03:34:58 -0700

    Tagging R3.4

--- package/wpe/wpeframework-amazon/wpeframework-amazon.mk
+++ package/wpe/wpeframework-amazon/wpeframework-amazon.mk
@@ -5,7 +5,7 @@
 #
 ################################################################################
 
-WPEFRAMEWORK_AMAZON_VERSION = 57c3e29840f3e29f44f81191c9f634b86f081754
+WPEFRAMEWORK_AMAZON_VERSION = R3.4
 WPEFRAMEWORK_AMAZON_SITE_METHOD = git
 WPEFRAMEWORK_AMAZON_SITE = git@github.com:Metrological/WPEPluginAmazon.git
 WPEFRAMEWORK_AMAZON_INSTALL_STAGING = YES
