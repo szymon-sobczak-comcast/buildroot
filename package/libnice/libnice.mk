99ce4599c0268df083948903021c24d04fcc8d5e
Philippe Normand <philn@igalia.com>
2023-01-19 17:50:14 +0000

    libnice: Bump to 0.1.21.

--- package/libnice/libnice.mk
+++ package/libnice/libnice.mk
@@ -4,29 +4,28 @@
 #
 ################################################################################
 
-LIBNICE_VERSION = 0.1.16
+LIBNICE_VERSION = 0.1.21
 LIBNICE_SITE = http://nice.freedesktop.org/releases
 LIBNICE_LICENSE = MPL-1.1 or LGPL-2.1
 LIBNICE_LICENSE_FILES = COPYING COPYING.MPL COPYING.LGPL
 LIBNICE_DEPENDENCIES = libglib2 host-pkgconf
 LIBNICE_INSTALL_STAGING = YES
-LIBNICE_CONF_OPTS = --without-gstreamer-0.10
+LIBNICE_CONF_OPTS += -Dintrospection=disabled -Dtests=disabled
 
 ifeq ($(BR2_PACKAGE_GNUTLS),y)
-LIBNICE_CONF_OPTS += --with-crypto-library=gnutls
+LIBNICE_CONF_OPTS += -Dcrypto-library=gnutls
 LIBNICE_DEPENDENCIES += gnutls
 else
 LIBNICE_CONF_OPTS += \
-	--with-crypto-library=openssl \
-	--with-openssl=$(STAGING_DIR)/usr
+	-Dcrypto-library=openssl
 LIBNICE_DEPENDENCIES += openssl
 endif
 
 ifeq ($(BR2_PACKAGE_GST1_PLUGINS_BASE),y)
-LIBNICE_CONF_OPTS += --with-gstreamer
+LIBNICE_CONF_OPTS += -Dgstreamer=enabled
 LIBNICE_DEPENDENCIES += gst1-plugins-base
 else
-LIBNICE_CONF_OPTS += --without-gstreamer
+LIBNICE_CONF_OPTS += -Dgstreamer=disabled
 endif
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
